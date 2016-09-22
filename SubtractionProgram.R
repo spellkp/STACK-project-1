a47849bb11dce26e9a5b9f50f36cd432616e65c6
Dustin Roten <Dustin Roten>
2016-09-22 16:28:42 -0400

    Yet another commit...

---
+++ SubtractionProgram.R
@@ -0,0 +1,84 @@
+###### Input Parameters #####
+
+centy <- 39.2846
+centx <- -96.1150
+
+AGL0m <- read.delim("0mcdump.txt", header = TRUE, sep = "", dec = ".")
+AGL200m <- read.delim("200mcdump.txt", header = TRUE, sep = "", dec = ".")
+
+resolution <- 30
+
+#############################
+
+require(rgl)
+require(patchPlot)
+source("http://www.phaget4.org/R/myImagePlot.R")
+
+y1 <- AGL0m[[5]]
+x1 <- AGL0m[[6]]
+Concentration0 <- AGL0m[[7]]
+metfile0 <- data.frame(x1, y1, Concentration0)
+
+y2 <- AGL200m[[5]]
+x2 <- AGL200m[[6]]
+Concentration1 <- AGL200m[[7]]
+metfile1 <- data.frame(x2, y2, Concentration1)
+
+###Bin Generator###
+
+Grid1 = NULL
+Grid2 = NULL
+
+minx1 <- round(min(min(x1), min(x2)), 0)-1
+maxx1 <- round(max(max(y1), max(y2)), 0)+1
+
+miny1 <- round(min(min(y1), min(y2)), 0)-1
+maxy1 <- round(max(max(y1), max(y2)), 0)+1
+
+Grid1 <- matrix(0, nrow = 400, ncol = 400)
+Grid2 <- matrix(0, nrow = 400, ncol = 400)
+
+#####
+
+for (i in 1:400) {
+  
+  for (j in 1:400) {
+    
+    Grid1[i,j] = mean(metfile0$Concentration0[metfile0$x1 >= centx+(-20+0.1*j) & metfile0$x1 
+                                              <= centx+(-20+0.1*(j+1)) & metfile0$y1 >= centy+(-20+0.1*i) 
+                                              & metfile0$y1 <= centy+(-20+0.1*(i+1))])
+    
+  }
+  
+}
+
+Grid1[is.nan(Grid1)] <- 0
+
+#####
+
+for (k in 1:400) {
+  
+  for (l in 1:400) {
+    
+    Grid2[k,l] = mean(metfile1$Concentration1[metfile1$x2 >= centx+(-20+0.1*l) & metfile1$x2 
+                                              <= centx+(-20+0.1*(l+1)) & metfile1$y2 >= centy+(-20+0.1*k) 
+                                              & metfile1$y2 <= centy+(-20+0.1*(k+1))])
+    
+  }
+  
+}
+
+Grid2[is.nan(Grid2)] <- 0
+
+#####
+
+GridPlot <- as.matrix(200*(Grid2-Grid1)/(Grid2+Grid1))
+GridPlot[is.nan(GridPlot)] <- 0
+
+myImagePlot(Grid1, title = c("Concentration at 0m AGL"))
+myImagePlot(Grid2, title = c("Concentration at 200m AGL"))
+
+myImagePlot(GridPlot, title = c("Dispersion % Difference"))
+mean(GridPlot)
+# rgl.surface(1:(maxx1-minx1), 1:(maxy1-miny1), GridPlot)
+
