e82dd504d8b13071d32f731b4ccbe3a61cc02f64
Dustin Roten <Dustin Roten>
2016-09-24 21:58:35 -0400

    Show both grids at once

--- SingleGridPlot.R
+++ SingleGridPlot.R
@@ -3,3 +3,12 @@ names(dd2) <- c('x','y','Concentration')
 
 d2 <- ggplot(dd2, aes(x,y,z='Concentration'))
 d2 + geom_tile(aes(fill=Concentration))  + scale_fill_gradient2(low="white", high="red")
+
+
+
+
+dd2 <- melt(abs(log10(Grid2)))
+names(dd2) <- c('x','y','Concentration')
+
+d2 <- ggplot(dd2, aes(x,y,z='Concentration'))
+d2 + geom_tile(aes(fill=Concentration))  + scale_fill_gradient2(low="white", high="red")
