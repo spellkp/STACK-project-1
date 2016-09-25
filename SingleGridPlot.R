42ae807233708c436819a0c8f194118372bef6e2
Dustin Roten <Dustin Roten>
2016-09-24 20:54:18 -0400

    Use for single grids

---
+++ SingleGridPlot.R
@@ -0,0 +1,5 @@
+dd2 <- melt(abs(log10(Grid1)))
+names(dd2) <- c('x','y','Concentration')
+
+d2 <- ggplot(dd2, aes(x,y,z='Concentration'))
+d2 + geom_tile(aes(fill=Concentration))  + scale_fill_gradient2(low="white", high="red")
