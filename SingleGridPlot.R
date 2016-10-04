6e1c32b32edd8a87782a20d8e8ebf2577f064953
Dustin Roten <Dustin Roten>
2016-10-04 01:13:56 -0400

    Fixing gradient...

--- SingleGridPlot.R
+++ SingleGridPlot.R
@@ -1,14 +1,31 @@
-dd2 <- melt(abs(log10(Grid1)))
-names(dd2) <- c('x','y','Concentration')
+dd2 <- melt(Grid1)
+
+div1 <- (max(AGL0m$CO2)-min(AGL0m$CO2))/6
+a <- min(AGL0m$CO2)
+b <- min(AGL0m$CO2) + div1
+c <- b + div1
+d <- c + div1
+e <- d + div1
+f <- e + div1
+g <- f + div1
 
 d2 <- ggplot(dd2, aes(x,y,z='Concentration'))
-d2 + geom_tile(aes(fill=Concentration))  + scale_fill_gradient2(low="white", high="red")
+d2 + geom_tile(aes(fill=Concentration)) +
+  scale_fill_manual(breaks = c("\[-inf,a)", "\[a,b)", "\[b,c)", "\[c,d)", "\[d,e)", "\[e,f)", "\[f,g)", "\[g,inf)"), values = c("black", "green", "darkgreen", "blue", "darkblue", "red", "darkred", "deepred"))
+  
+##########################################################
 
-
-
-
-dd3 <- melt(abs(log10(Grid2)))
+dd3 <- melt(Grid2)
 names(dd3) <- c('x','y','Concentration')
 
+div2 <- (max(AGL200m$CO2)-min(AGL200m$CO2))/6
+a1 <- min(AGL200m$CO2)
+b1 <- min(AGL200m$CO2) + div2
+c1 <- b1 + div2
+d1 <- c1 + div2
+e1 <- d1 + div2
+f1 <- e1 + div2
+g1 <- f1 + div2
+
 d3 <- ggplot(dd3, aes(x,y,z='Concentration'))
-d3 + geom_tile(aes(fill=Concentration))  + scale_fill_gradient2(low="white", high="red")
+d3 + geom_tile(aes(fill=Concentration))  + 
