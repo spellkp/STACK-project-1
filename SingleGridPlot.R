4657999e9cff1c49b3c1641367e6e3dccc279a40
Dustin Roten <Dustin Roten>
2016-10-07 16:47:54 -0400

    It's working!

--- SingleGridPlot.R
+++ SingleGridPlot.R
@@ -1,33 +1,38 @@
 dd2 <- melt(Grid1)
 
 div1 <- (max(dd2$value)-min(dd2$value))/6
-a <- min(dd2$value)
-b <- min(dd2$value) + div1
-c <- b + div1
-d <- c + div1
-e <- d + div1
-f <- e + div1
-g <- f + div1
+a <- 0
+b <- 1.00e-16
+c <- 1.00e-15
+d <- 1.00e-14
+e <- 1.00e-13
+f <- 1.00e-12
+g <- 1.00e-11
 
-value2 <- cut(dd2$value, breaks = c(a:b, b:c, c:d, d:e, e:f, f:g), right = FALSE)
 
-d2 <- ggplot(dd2, aes(x,y,z='Concentration'))
-d2 + geom_tile(aes(fill=value)) +
-  scale_fill_manual(breaks = value2, values = c("green", "darkgreen", "blue", "darkblue", "red", "darkred"))
+
+dd2$cutdata <- cut(dd2$value, breaks = c(a, b, c, d, e, f, g))
+colnames(dd2)[4] <- "Concentration"
+
+vals=c(a:b, b:c, c:d, d:e, e:f, f:g)
+
+d2 <- ggplot(dd2, aes(Var1, Var2, cutdata))
+d2 + geom_tile(aes(fill=cutdata)) +
+  scale_colour_gradientn(colors = "green") +
+  labs(x = "0.1 Degrees", y = "0.1 Degrees", title = "Gridded Dispersion Concentration 0m AGL") +
+  guides(fill=guide_legend(title="Concentration"))
   
 ##########################################################
 
 dd3 <- melt(Grid2)
-names(dd3) <- c('x','y','Concentration')
 
-div2 <- (max(AGL200m$CO2)-min(AGL200m$CO2))/6
-a1 <- min(AGL200m$CO2)
-b1 <- min(AGL200m$CO2) + div2
-c1 <- b1 + div2
-d1 <- c1 + div2
-e1 <- d1 + div2
-f1 <- e1 + div2
-g1 <- f1 + div2
+div2 <- (max(dd3$value)-min(dd3$value))/6
 
-d3 <- ggplot(dd3, aes(x,y,z='Concentration'))
-d3 + geom_tile(aes(fill=Concentration))  + 
+dd3$cutdata <- cut(dd3$value, breaks = c(a, b, c, d, e, f, g))
+colnames(dd3)[4] <- "Concentration"
+
+d3 <- ggplot(dd3, aes(Var1, Var2, cutdata))
+d3 + geom_tile(aes(fill=cutdata)) +
+  scale_colour_gradientn(colours = c("firebrick4","orange","yellow", "green", "darkblue", "purple")) +
+  labs(x = "0.1 Degrees", y = "0.1 Degrees", title = "Gridded Dispersion Concentration 200m AGL", legend = "Concentration") +
+  guides(fill=guide_legend(title="Concentration"))
