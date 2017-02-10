b059f63d6dba3ac68a59296b864322f3ea06b3bc
Dustin Roten <rotendd@appstate.edu>
2017-02-10 18:14:06 -0500

    html "complete"

--- ControlFileBuilder/app.R
+++ ControlFileBuilder/app.R
@@ -1,27 +1,40 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
-
-# It's Build-A-Bear for Dispersion Modeling
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("HYSPLIT Control Parameters"),
-   
-   # Input the data!
-  column(4, textInput("01", label = h4("Dispersion Start- YY MM DD HH"), value = ""), placeholder = "YY MM DD HH"))
-
-  column(4, selectInput("02", label = h4("Number of Point Sources"), choices = c("1", "2", "3", "4", "5"),
-                          selected = NULL, multiple = FALSE, selectize = FALSE))
+ui <- shinyUI(fluidPage(titlePanel("HYSPLIT Control File"), mainPanel(
   
+  fluidPage(
+  
+  textInput("01", label = h5("Dispersion Start (YY MM DD HH)"), value = ""),
+  
+  textInput("11", label = h5("Pollutant Name (4 Characters Max)"), value = ""),
 
-# Run the application 
-runApp()
+  textInput("12", label = h5("Average Emission Rate (mass/hr)"), value = ""),
+    
+  selectInput("02", label = h5("Number of Point Sources"), choices = c("1", "2", "3"),
+                      selected = NULL, multiple = FALSE, selectize = FALSE),
 
+  textInput("03", label = h6("Point Source 1 Parameters (lat, lon, height(AGL), exhaust velocity (m/s), area (m^2)"), value = ""),
+    
+  textInput("04", label = h6("Point Source 2 Parameters (lat, lon, height(AGL), exhaust velocity (m/s), area (m^2)"), value = ""),
+  
+  textInput("05", label = h6("Point Source 3 Parameters (lat, lon, height(AGL), exhaust velocity (m/s), area (m^2)"), value = ""),
+  
+  textInput("06", label = h5("Emission Length (hr)"), value = ""),
+    
+  textInput("08", label = h5("Lower Model Level (m)"), value = ""),
+  
+  textInput("09", label = h5("Upper Model Level (m)"), value = "")
+
+
+## Number of MET files go here!
+
+## Path to MET files go here!
+
+))))
+
+server <- function(input, output) {
+  
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
