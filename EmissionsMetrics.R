5563afc19c9aa6605df6b99760ece8086111dea6
Dustin Roten <Dustin Roten>
2017-03-07 21:17:55 -0500

    Thank God I found this!

---
+++ EmissionsMetrics.R
@@ -0,0 +1,155 @@
+library(ggplot2)
+library(dplyr)
+
+#Read in models to compare
+ModelA <- read.csv("A-2012")
+ModelA <- ModelA[2:6]
+ModelE <- read.csv("E-2012")
+ModelE <- ModelE[2:6]
+
+#Comparing maximum concentration
+Day = NULL
+PercentDifference = NULL
+
+for (i in 2:366) {
+
+  Day[i] <- i
+  tempModelA <- subset(ModelA, ModelA$Day == i)
+  tempModelE <- subset(ModelE, ModelE$Day == i)
+  PercentDifference[i] <- ((max(tempModelE$Conc)-max(tempModelA$Conc))/mean(c(max(tempModelA$Conc), max(tempModelE$Conc))))*100
+
+}
+
+MaxConc <- data.frame(Day, PercentDifference)
+
+ggplot(data = MaxConc, aes(x = Day, y = PercentDifference)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  xlab("Day of 2012") +
+  ylab("Percent Difference (%)") +
+  ggtitle("Jeffrey Energy Center (2012): \n Percent Difference in Simulated Maximum Concentration per Day") +
+  theme_bw()
+
+#Comparing location of COM (Unweighted)
+Day2 = NULL
+PercentDifference2 = NULL
+
+for (i in 2:366) {
+  
+  Day2[i] <- i
+  tempModelA <- subset(ModelA, ModelA$Day == i)
+  tempModelE <- subset(ModelE, ModelE$Day == i)
+  
+  LatA <- mean(tempModelA$Lat)
+  LonA <- mean(tempModelA$Lon)
+  LatE <- mean(tempModelE$Lat)
+  LonE <- mean(tempModelE$Lon)
+  
+  PercentDifference2[i] <- 200*sqrt(((LatA-LatE)/(LatA+LatE))^2 + ((LonA-LonE)/(LonA+LonE))^2)
+  
+}
+
+CenterConc <- data.frame(Day2, PercentDifference2)
+
+ggplot(data = CenterConc, aes(x = Day2, y = PercentDifference2)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  xlab("Day of 2012") +
+  ylab("Percent Difference (%)") +
+  ggtitle("Jeffrey Energy Center (2012): \n Percent Difference in Center of Dispersion (Unweighted) per Day") +
+  theme_bw()
+
+#Comparing location of COM (Weighted)
+Day3 = NULL
+PercentDifference3 = NULL
+
+for (i in 2:366) {
+  
+  Day3[i] <- i
+  tempModelA <- subset(ModelA, ModelA$Day == i)
+  tempModelE <- subset(ModelE, ModelE$Day == i)
+  
+  LatA_bar <- sum(tempModelA$Lat*tempModelA$Conc)/sum(tempModelA$Conc)
+  LonA_bar <- sum(tempModelA$Lon*tempModelA$Conc)/sum(tempModelA$Conc)
+  LatE_bar <- sum(tempModelE$Lat*tempModelE$Conc)/sum(tempModelE$Conc)
+  LonE_bar <- sum(tempModelE$Lat*tempModelE$Conc)/sum(tempModelE$Conc)
+  
+  PercentDifference3[i] <- 200*sqrt(((LatA_bar-LatE)/(LatA_bar+LatE))^2 + ((LonA-LonE)/(LonA+LonE))^2)
+  
+}
+
+CenterConc2 <- data.frame(Day3, PercentDifference3)
+
+ggplot(data = CenterConc2, aes(x = Day2, y = PercentDifference3)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  xlab("Day of 2012") +
+  ylab("Percent Difference (%)") +
+  ggtitle("Jeffrey Energy Center (2012): \n Percent Difference in Center of Dispersion (Weighted) per Day") +
+  theme_bw()
+
+#Comparing area of dispersion
+Day4 = NULL
+PercentDifference4 = NULL
+
+for (i in 2:366) {
+
+  tempModelA <- subset(ModelA, ModelA$Day == i)
+  tempModelA <- as.data.frame(tempModelA[order(tempModelA$Lon, tempModelA$Lat),])
+
+  tempModelE <- subset(ModelE, ModelE$Day == i)
+  tempModelE <- as.data.frame(tempModelE[order(tempModelE$Lon, tempModelE$Lat),])
+  
+  AreaA <- matrix(data = 0, nrow = (max(tempModelA$Lon)-min(tempModelA$Lon))/0.01,
+                                    (max(tempModelA$Lat)-min(tempModelA$Lat))/0.01)
+  AreaE <- matrix(data = 0, nrow = (max(tempModelE$Lon)-min(tempModelE$Lon))/0.01,
+                                    (max(tempModelE$Lat)-min(tempModelE$Lat))/0.01)
+  
+#Begin finding the area of the dispersion for Day[i], ModelA
+deltaLat1 <- (max(tempModelA$Lat)-min(tempModelA$Lat))/0.01
+deltaLon1 <- (max(tempModelA$Lon)-min(tempModelA$Lon))/0.01
+
+  for (q in 0:deltaLat1) {  
+   
+   for (j in 0:deltaLon1) {
+  
+    test1 <- subset(tempModelA, tempModelA$Lon >= (min(tempModelA$Lon)+(0.01*j)) & tempModelA$Lon < (min(tempModelA$Lon)+0.01*(j+1)))
+    test1 <- subset(test1, test1$Lat >= (min(tempModelA$Lat)+(0.01*q)) & test1$Lat < (min(tempModelA$Lat)+0.01*(q+1)))
+     
+    if (nrow(test1) != 0) {AreaA[j,q] <- 1}
+     else {AreaA[j,q] <- 0}
+  
+    print(paste("Model A", "     ", "Year:", round(i/366, 2)*100, "%", "     ","Latitude Complete:", round(q/deltaLat1, 2)*100, "%", "     ",
+                "Longitude Complete:", round(j/deltaLon1, 2)*100, "%", sep = ""))
+     
+   }
+
+    }
+  
+#Begin finding the area of the dispersion for Day[i], ModelE
+deltaLat2 <- (max(tempModelE$Lat)-min(tempModelE$Lat))/0.01
+deltaLon2 <- (max(tempModelE$Lon)-min(tempModelE$Lon))/0.01
+
+for (q in 0:deltaLat2) {  
+    
+    for (j in 0:deltaLon2) {
+      
+      test2 <- subset(tempModelE, tempModelE$Lon >= (min(tempModelE$Lon)+(0.01*j)) & tempModelE$Lon < (min(tempModelE$Lon)+0.01*(j+1)))
+      test2 <- subset(test2, test2$Lat >= (min(tempModelE$Lat)+(0.01*q)) & test2$Lat < (min(tempModelE$Lat)+0.01*(q+1)))
+      
+      if (nrow(test2) != 0) {AreaE[j,q] <- 1}
+      else {AreaE[j,q] <- 0}
+      
+      print(paste("Model B", "     ", "Year:", round(i/366, 2)*100, "%", "     ","Latitude Complete:", round(q/deltaLat1, 2)*100, "%", "     ",
+                  "Longitude Complete:", round(j/deltaLon1, 2)*100, "%", sep = ""))
+    }
+  
+  }
+  
+Day4[i] <- i
+PercentDifference4[i] <- 100*(sum(AreaE, na.rm = TRUE)-sum(AreaA, na.rm = TRUE))/mean(c(sum(AreaE), sum(AreaA)))
+print(paste("Percent Complete:", " ", 100*(i/366), sep = ""))
+
+}
+
+AreaComp <- data.frame(Day4, PercentDifference4)
