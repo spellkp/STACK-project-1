613e4f967af4f6f8226d73af69d62a18cd8d5c43
rotendd <rotendd@appstate.edu>
2017-03-10 17:33:43 -0500

    Progress Saved

--- EmissionsMetricsGamma.R
+++ EmissionsMetricsGamma.R
@@ -4,13 +4,13 @@
 IfPar <- 1            #1='Yes'; 0='No'#
 FreeCores <- 1        #Number of cores available during computation
 
-NumStacks <- 3        #Number of stacks at location
-
-StackLoc <- rbind(#Insert c(LAT,LON); more can be added.
-                  c(     ,     )
-                  c(     ,     )
-                  c(     ,     )
+StackLoc <- rbind(                               #Insert c(LAT,LON); more can be added.
+                  c(39.28684, -96.11821),
+                  c(39.28681, -96.11721),
+                  c(39.28681, -96.11618)
                   )
+
+eGRIDLoc <- c(39.2865, -96.1172)
 
 Model1 <- "A-2012"    #Name of dataset for Model 1; MUST BE CSV
 Model2 <- "E-2012"    #Name of dataset for Model 2; MUST BE CSV
@@ -30,24 +30,63 @@ library(doParallel)
 #####################################
 ##### Initialize Parallel Cores #####
 #####################################
-if (IfPar == 1) {
-  cl <- detectCores()-FreeCores
+if (IfPar == 1)
+  {cl <- detectCores()-FreeCores
   registerDoParallel(cl)
-}
-else {print("Serial Computation")}
-#####################################
+} else {print("Serial Computation")}
 
 #####################################
-##### Read in Data ##################
-#####################################
+
+#############################################
+##### Analysis of Maximum Concentration #####
+#############################################
+
+##### Model 1 (Single Stack) ########
+
 Model1 <- read.csv(Model1)[2:6]
+
+MaxConcDist1 = 
+  foreach(i=2:366, .combine = rbind) %dopar% {
+  
+      tempModel1 <- subset(Model1, Model1$Day == i)
+  
+      Index <- which(tempModel1$Conc == max(tempModel1$Conc))
+      Lat <- tempModel1$Lat[Index] - eGRIDLoc[1]
+      Lon <- tempModel1$Lon[Index] - eGRIDLoc[2]
+      Conc <- tempModel1$Conc[Index]
+      cbind(Lat, Lon, Conc)
+      
+  }
+
+
+ggplot(data = as.data.frame(MaxConcDist1), aes(x = Lon, y = Lat)) +
+  geom_point(aes(color=Conc)) +
+  theme_bw()
+
+ggplot(data = as.data.frame(MaxConcDist1), aes(x = c(2:366), y = Conc)) +
+  geom_point() +
+  geom_smooth(se = FALSE) +
+  theme_bw()
+
+##### Model 2 (Full Model) ##########
+
 Model2 <- read.csv(Model2)[2:6]
 
-Analysis1 = NULL
+MaxConcDist2 = 
+  foreach(i=2:366, .combine = rbind) %dopar% {
+    
+    tempModel2 <- subset(Model2, Model2$Day == i)
+    
+    Index <- which(tempModel2$Conc == max(tempModel2$Conc))
+    Lat <- tempModel2$Lat[Index] - eGRIDLoc[1]
+    Lon <- tempModel2$Lon[Index] - eGRIDLoc[2]
+    Conc <- tempModel2$Conc[Index]
+    cbind(Lat, Lon, Conc)
+    
+  }
 
-Metric1 = foreach(i=2:366, .combine = rbind) %dopar% {
-  
-  tempModel1 <- subset(Model1, Model1$Day == i)
-  tempModel2 <- subset(Model2, Model2$Day == i)
-  
-}
+
+ggplot(data = MaxConcDist2, aes(x = Lon, y = Lat)) +
+  geom_plot(aes(color=Conc)) +
+  theme_bw()
+
