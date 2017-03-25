516bd9275c6c245980eb116279dd1806aa23cdc8
Dustin Roten <Dustin Roten>
2017-03-25 16:26:04 -0400

    Hopefully temporary additions

--- FullMetric.R
+++ FullMetric.R
@@ -1,3 +1,5 @@
+setwd('~/ResearchScripts/STACK-project')
+
 #####################################
 ##### Initialize Parameters #########
 #####################################
@@ -11,7 +13,7 @@ FreeCores <- 1        #Number of cores available during computation
   
 eGRIDLoc <- c(39.2865, -96.1172)
 
-Model1 <- "JEC-B-2012"    #Name of dataset for Model 1; MUST BE CSV
+Model1 <- "JEC-A-2012"    #Name of dataset for Model 1; MUST BE CSV
 Model2 <- "JEC-E-2012"    #Name of dataset for Model 2; MUST BE CSV
 
 #####################################
@@ -73,31 +75,79 @@ for (i in 2:366) {
     
     Comp2PercentDifference[i] <- (CentModel2 - CentModel1)/mean(c(CentModel2, CentModel1))
 }
- 
-
-##### This is stupid... Get it together R programmers! #####
 
 
-for (i in 2:366) {   
-    #Area Comparison
+
+
+##### Area Comparison #####
+DispArea1 = 
+  foreach(i=2:366, .combine = rbind) %dopar% {
+    
     tempModel1 <- subset(Model1, Model1$Day == i)
-    tempModel2 <- subset(Model2, Model2$Day == i)
-  
+    
     s <- interp(x = tempModel1$Lon, y = tempModel1$Lat, z = tempModel1$Conc)
     new_s <- s$z*0
     new_s <- new_s+1
-    AreaModel1 <- sum(new_s, na.rm = TRUE)*12321
+    sum(new_s, na.rm = TRUE)*12321
+  }
+
+DispArea1 <- as.data.frame(DispArea1)
+
+
+
+DispArea2 = 
+  foreach(i=2:366, .combine = rbind) %dopar% {
+    
+    tempModel2 <- subset(Model2, Model2$Day == i)
     
     s <- interp(x = tempModel2$Lon, y = tempModel2$Lat, z = tempModel2$Conc)
     new_s <- s$z*0
     new_s <- new_s+1
-    AreaModel2 <- sum(new_s, na.rm = TRUE)*12321
-    
-    Comp3PercentDifference[i] <- (AreaModel2 - AreaModel1)/mean(c(AreaModel1, AreaModel2))
-    
+    sum(new_s, na.rm = TRUE)*12321
   }
 
+DispArea2 <- as.data.frame(DispArea2)
 
-PercentDifferenceData <- data.frame(Comp1PercentDifference, Comp2PercentDifference, Comp3PercentDifference, 
-                                    sqrt(Comp1PercentDifference^2 + Comp2PercentDifference^2 + Comp3PercentDifference^2))
+Comp3PercentDifference <- (DispArea2 - DispArea1)/mean(c(DispArea2$V1,DispArea1$V1))
 
+##### Build the dataframe with all percent differences in it #####
+
+PercentDifferenceData <- data.frame(c(2:366), 100*Comp1PercentDifference[2:366], 100*Comp2PercentDifference[2:366], 100*Comp3PercentDifference$V1,
+                                    100*sqrt(Comp1PercentDifference[2:366]^2 + Comp2PercentDifference[2:366]^2 + Comp3PercentDifference$V1^2))
+
+names(PercentDifferenceData) <- c("Day", "Metric 1", "Metric 2", "Metric 3", "Overall")
+
+
+
+##### Plots #####
+ggplot(data = PercentDifferenceData, aes(x = Day, y = Overall)) +
+  geom_point() +
+  xlab("Day") +
+  ylab("Percent Difference (%)") +
+  geom_smooth(se = FALSE) +
+  theme_bw()
+
+##########
+
+MaxDay <- PercentDifferenceData$Day[PercentDifferenceData$Overall == max(PercentDifferenceData$Overall)]-1
+
+PlotModel1 <- subset(Model1, Day == MaxDay)
+PlotModel2 <- subset(Model2, Day == MaxDay)
+
+al1 = get_map(location = c(lon = eGRIDLoc[2], lat = eGRIDLoc[1]), zoom = 06, maptype = 'satellite')
+al1MAP = ggmap(al1)
+al1MAP + geom_tile(data = PlotModel1, aes(x = Lon, y = Lat, fill = Conc)) +
+  scale_fill_gradient(limits=c(min(min(PlotModel1$Conc), min(PlotModel2$Conc)), 
+                               max(max(PlotModel1$Conc), max(PlotModel2$Conc))), low = "yellow", high = "red") +
+  xlab("Longitude") +
+  ylab("Latitude") 
+
+##########
+
+al1 = get_map(location = c(lon = eGRIDLoc[2], lat = eGRIDLoc[1]), zoom = 06, maptype = 'satellite')
+al1MAP = ggmap(al1)
+al1MAP + geom_tile(data = PlotModel2, aes(x = Lon, y = Lat, fill = Conc)) +
+  scale_fill_gradient(limits=c(min(min(PlotModel1$Conc), min(PlotModel2$Conc)), 
+                               max(max(PlotModel1$Conc), max(PlotModel2$Conc))), low = "yellow", high = "red") +
+  xlab("Longitude") +
+  ylab("Latitude") 
