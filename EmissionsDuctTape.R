b372deeac11a18721815a3d47f504ad69472dee9
Dustin Roten <Dustin Roten>
2017-04-22 15:36:13 -0400

    Setup for JEC

--- EmissionsDuctTape.R
+++ EmissionsDuctTape.R
@@ -1,4 +1,4 @@
-ModelType <- "B"
+ModelType <- "E"
 MonthName <- c("January",
                "February",
                "March",
@@ -72,7 +72,7 @@ MonthName <- c("January",
 
 for (i in 1:12) {
   
-  filepath <- paste("JSC-2012/", ModelType, "-", MonthName[i], sep = "")
+  #filepath <- paste("JSC-2012/", ModelType, "-", MonthName[i], sep = "")
   files <- dir(path = filepath, pattern = "", full.names = TRUE, recursive = TRUE)
   
   for (j in 1:length(files)) {
